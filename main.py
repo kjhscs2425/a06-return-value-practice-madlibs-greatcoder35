221b73ff7b618c1b710845aa50b78752aef77ad2
Jordan Nash <jordangabrielnash@gmail.com>
2024-11-07 10:52:55 -0800

    done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun: ")
+pluralnoun0 = input("Give me a plural noun: ")
+person0 = input("Give me the name of a female person in the room: ")
+adjective1 = input("Give me a new adjective: ")
+clothing0 = input("Give me the name of an singular article of clothing: ")
+noun1 = input("Give me a new noun: ")
+city0 = input("Give me the name of a city: ")
+pluralnoun1 = input("Give me a new plural noun: ")
+adjective2 = input("Give me a new adjective: ")
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+story = f"There are many {adjective0} ways to teach a {noun0} to read. First, you could ask for recommendations from your friends and {pluralnoun0}. Just don't ask Aunt {person0} --she only reads {adjective1} books with {clothing0}-ripping goddesses on the cover. If your friends and family are no help, try checking out the {noun1} Review in The {city0} Times. If the {pluralnoun1} featured there are too {adjective2} for your taste, try something a little"
 print(story)
